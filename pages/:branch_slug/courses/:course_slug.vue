13efa4f32311189b99638a2e47a15b0ecf61538e
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-09-23 13:25:01 +0530

    completed courses along with payment gateway

--- pages/:branch_slug/courses/:course_slug.vue
+++ pages/:branch_slug/courses/:course_slug.vue
@@ -70,7 +70,7 @@
                                             <form id="contactForm" @submit.prevent="handleSubmit(formHandler)">
                                                 <div class="row">
                                                     <div class="col-12">
-                                                        <h3 class="user-title">Enrollment Form</h3>
+                                                        <h3 class="user-title">Fill in your details</h3>
                                                     </div>
                                                     <div class="col-lg-12 col-md-12 mb-2">
                                                         <div class="form-group">
@@ -194,7 +194,8 @@ export default {
             script: [{
                 type: 'application/ld+json',
                 innerHTML: this.course?.meta_scripts // <- set jsonld object in data or wherever you want
-            }],
+            },
+            { src: 'https://checkout.razorpay.com/v1/checkout.js' }],
             __dangerouslyDisableSanitizers: ['script'],
         }
     },
@@ -227,12 +228,13 @@ export default {
         async formHandler() {
             this.enrollmentLoading = true;
             try {
-                const response = await this.$publicApi.post(API_ROUTES.course + `/comment/${this.course?.id}/create`, {
+                const response = await this.$publicApi.post(API_ROUTES.course + `/${this.$route.params.course_slug}/branch/${this.$route.params.branch_slug}/enroll`, {
                     name: this.name,
                     email: this.email,
                     phone: this.phone,
                 });
                 this.$refs.form.reset()
+                this.loadRazorpay(response.data.enrollmentForm)
             } catch (err) {
                 this.$refs.form.setErrors({
                     email: err?.response?.data?.errors?.email?.length > 0 && err?.response?.data?.errors?.email[0],
@@ -246,6 +248,49 @@ export default {
                 this.enrollmentLoading = false;
             }
         },
+        loadRazorpay(data){
+            const options = {
+                key: this.$config.RAZORPAY_KEY_ID,
+                amount: data.discounted_amount*100,
+                currency: 'INR',
+                description: "Payment description",
+                order_id: data.razorpay_order_id,
+                prefill: {
+                name: data.name,
+                email: data.email,
+                contact: data.phone
+                },
+                theme: {
+                color: "#000000" // Set your website theme color
+                },
+                handler: async (response) => {
+                    await this.verifyPayment(response)
+                }
+            };
+
+            // eslint-disable-next-line no-undef
+            const rzp = new Razorpay(options);
+            rzp.open();
+        },
+        async verifyPayment(data){
+            const loading = this.$loading({
+                lock: true,
+                fullscreen: true,
+            });
+            try {
+                const response = await this.$privateApi.post('/api/v1/enrollment/verify',{
+                    razorpay_order_id: data.razorpay_order_id,
+                    razorpay_payment_id: data.razorpay_payment_id,
+                    razorpay_signature: data.razorpay_signature,
+                }); // eslint-disable-line
+                this.$toast.success(response.data.message)
+            } catch (err) {
+                if(err?.response?.data?.message) this.$toast.error(err?.response?.data?.message)
+                if(err?.response?.data?.error) this.$toast.error(err?.response?.data?.error)
+            } finally{
+                loading.close()
+            }
+        }
     },
 }
 </script>
